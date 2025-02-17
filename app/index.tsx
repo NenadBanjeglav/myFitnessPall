f67d09622d963cf871f8901e8e1ff4f95ff7e79c
Nenad <nenadmobmail@gmail.com>
2025-02-17 11:41:55 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import "../global.css";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="justify-center items-center flex-1">
+      <Text className="text-red-500">Dashboard</Text>
+    </View>
+  );
+}
