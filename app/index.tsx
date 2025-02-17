7d7512f75f0b08986cecc2df3b2ffe78275912a7
Nenad <nenadmobmail@gmail.com>
2025-02-17 12:46:11 +0100

    FoodListItem

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
 import "../global.css";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+
+import FoodListItem from "../components/FoodListItem";
 
 export default function App() {
   return (
-    <View className="justify-center items-center flex-1">
-      <Text className="text-red-500">Dashboard</Text>
+    <View className="justify-center items-center flex-1 p-4 gap-2">
+      <FoodListItem item={{ label: "Pizza", cal: "75", brand: "Dominos" }} />
+      <FoodListItem
+        item={{ label: "Chicken breast raw", cal: "120", brand: "Lidl" }}
+      />
     </View>
   );
 }
---
+++ components/FoodListItem.tsx
@@ -0,0 +1,26 @@
+import { View, Text } from "react-native";
+import React from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+interface FoodItem {
+  label: string;
+  cal: string;
+  brand: string;
+}
+
+const FoodListItem = ({ item }: { item: FoodItem }) => {
+  const { label, cal, brand } = item;
+  return (
+    <View className="bg-gray-300 p-4 rounded-md w-full gap-2 flex-row justify-between items-center">
+      <View className="flex-1 gap-2">
+        <Text className="font-bold">{label}</Text>
+        <Text className="color-gray-500">
+          {cal} cal, {brand}
+        </Text>
+      </View>
+      <AntDesign name="pluscircleo" size={24} color="blue" />
+    </View>
+  );
+};
+
+export default FoodListItem;
